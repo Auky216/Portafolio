fc36902c7fbe444392fcfa95fe0fd6f7bdbf9138
adrianauquiperez <adrian.auqui@utec.edu.pe>
2025-01-22 15:16:04 -0500

    new design

--- src/app/components/home/header.tsx
+++ src/app/components/home/header.tsx
@@ -1,50 +1,19 @@
-import { FaArrowLeft } from "react-icons/fa6";
-import { FaArrowRight } from "react-icons/fa";
-import { AiFillNotification } from "react-icons/ai";
-import { FaEnvelopeOpen } from "react-icons/fa";
-import { BsBadgeVrFill } from "react-icons/bs";
-import { MdScreenshotMonitor } from "react-icons/md";
+import { FaApple } from "react-icons/fa6";
 
 
 export default function Header() {
 
     return(
-        <div className="bg-gradient-to-b from-[#151e2c] to-[#1e2530] h-14 ">
-        <div className="flex flex-row gap-4 h-1/4 p-2">
-          <p className="text-[#5d606e] text-[10px]">Steam</p>
-          <p className="text-[#5d606e] text-[10px]">View</p>
-          <p className="text-[#5d606e] text-[10px]">Friend</p>
-          <p className="text-[#5d606e] text-[10px]">Games</p>
-          <p className="text-[#5d606e] text-[10px]">Help</p>
-
-          <div className="ml-auto flex flex-row gap-4 items-center p-3">
-            <div className="bg-[#192b3a]">
-            <AiFillNotification />
-
-            </div>  
-            <div className="bg-[#182b3c]">
-            <FaEnvelopeOpen />
-            </div>
-            <div className="flex flex-row items-center bg-[#182b3c]">
-                <img className="h-[20px] border-r-4 border-r-[#3e6983]" src="https://avatars.githubusercontent.com/u/91244347?v=4"/>
-                <p className="text-[10px] px-2">Auky216</p>
-            </div>
-            <BsBadgeVrFill />
-            <MdScreenshotMonitor />
-
-
-          </div>
-        </div>
-        <div className="h-3/4 flex flex-row gap-4 items-center p-2">
-        <FaArrowLeft />
-        <FaArrowRight />
-        <p className="text-white text-xl">STORE</p>
-        <p className="text-white text-xl">LIBRARY</p>
-        <p className="text-white text-xl">COMMUNITY</p>
-        <p className="text-white text-xl">AUKY216</p>
-
-
-        </div>
+        
+      <div className="flex gap-4 p-2">
+        <FaApple />
+        <p>Finder</p>
+        <p>File</p>
+        <p>Edit</p>
+        <p>View</p>
+        <p>Go</p>
+        <p>Window</p>
+        <p>Help</p>
       </div>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,14 @@
-import Image from "next/image";
-import Profile from "./components/home/profile";
-import Language from "./components/home/language";
-import Experience from "./components/home/experience";
-import Proyects from "./components/home/proyects";
 import Header from "./components/home/header";
-
 import { FaMedal } from "react-icons/fa";
 
-export default function Home() {
-  return (  
-    <div>
+export default function Page() {
+  return (
+    <div className="bg-cover bg-center bg-[url('https://applescoop.org/image/wallpapers/mac/macos-11-big-sur-stock-22-09-2024-1727045363-hd-wallpaper.jpg')] h-screen w-screen overflow-hidden">
+
       <Header />
-      
-    <div className="bg-[url('https://images4.alphacoders.com/138/thumb-1920-1383046.jpg')]
-     bg-cover bg-center h-screen w-screen flex justify-center items-center ">
-      
-      <div className="bg-black/75 h-full w-1/2 p-5 flex flex-row" >
-
-        <div className="w-3/4 flex flex-col gap-4 pr-5">
-          <div className="flex">
-            <Profile />
-            <div className="p-2">
-              <h1 className="text-white text-lm">Adrian Antonio Auqui Perez</h1>
-              <p className="text-white text-xs">🇵🇪 Perú, Lima</p>
-              <p className="text-white text-xs pt-5">This is a small description about me :D</p>
-              </div>
-          </div>
-
-          <Language />
-          <Experience />
-          <Proyects />
-          
-        </div>
-
-        <div className="w-1/4 p-2 border border-white">
-        <div className="flex items-center gap-2">
-            <h1 className="text-white">Level</h1>
-            <FaMedal />
-
-          </div>
-        </div>
-          
-      </div>
-    </div>
-    <div className="h-16">
-<h1>final           </h1>
-    </div>
+      <h1 className="text-white text-4xl flex justify-center items-center h-screen">
+        hola
+      </h1>
     </div>
   );
 }
