5036ba7efbe7149f2b393e68b7cc16349754de94
adrianauquiperez <adrian.auqui@utec.edu.pe>
2025-01-22 18:21:38 -0500

    add useState in navbar

--- src/app/components/home/navbar.tsx
+++ src/app/components/home/navbar.tsx
@@ -1,4 +1,9 @@
-export default function Navbar() {
+"use client";
+type NavbarProps = {
+    onAplicationClick : (app : string) => void;
+};
+
+export default function Navbar({onAplicationClick} : NavbarProps) {
     return (
       <div className="flex items-center justify-center w-full p-4">
         {/* Contenedor con fondo blanco que envuelve los iconos */}
@@ -6,26 +11,32 @@ export default function Navbar() {
           <img
             className="size-16 transition-transform transform hover:scale-125"
             src="https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/8204ffaf2c6f9f46a1a803a96c91e7d5_low_res_Safari.png"
+            onClick={() => onAplicationClick("Safari")}
           />
           <img
             className="size-16 transition-transform transform hover:scale-125"
             src="https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/c042465c64b6c0c06ef0525e5a7bcb12_low_res_App_Store.png"
+            onClick={() => onAplicationClick("App Store")}
           />
           <img
             className="size-16 transition-transform transform hover:scale-125"
             src="http://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/a2b41bcb7e94def55c6cfa9ecf586b13_fCGMiEOWax.png"
+            onClick={() => onAplicationClick("LinkedIn")}
           />
           <img
             className="size-16 transition-transform transform hover:scale-125"
             src="https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/ba3ad96efac721276cee376c99b9388f_low_res_Spotify_black.png"
+            onClick={() => onAplicationClick("Spotify")}
           />
           <img
             className="size-16 transition-transform transform hover:scale-125"
             src="https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/b0eaa96aed8d965169a2b0c23f5dad04_low_res_Github_Desktop.png"
+            onClick={() => onAplicationClick("GitHub")}
           />
           <img
             className="size-16 transition-transform transform hover:scale-125"
             src="https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/7abc86d5d547522303785e02f3a49f43_low_res_Folder_Peek.png"
+            onClick={() => onAplicationClick("Folder")}
           />
         </div>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+"use client";
+
+import { useState } from "react";
+
 import Header from "./components/home/header";
 import { FaMedal } from "react-icons/fa";
 import Window from "./components/window/windows";
 import Navbar from "./components/home/navbar";
 
 export default function Page() {
+  // Estado para almacenar el nombre del icono seleccionado
+  const [selectedAplication, setSelectedAplication] = useState<string>("");
+
+  // Función para actualizar el estado con el nombre del icono
+  const handleAplicationClick = (iconName: string) => {
+    setSelectedAplication(iconName);
+  };
+
   return (
     <div className="bg-cover bg-center bg-[url('https://applescoop.org/image/wallpapers/mac/macos-11-big-sur-stock-22-09-2024-1727045363-hd-wallpaper.jpg')] min-h-screen flex flex-col overflow-hidden">
       {/* Componente Header */}
@@ -11,12 +23,12 @@ export default function Page() {
       
       {/* Contenido de la página */}
       <div className="flex-1">
-        hola
+        <p>hola {selectedAplication}</p>
         <Window />
       </div>
 
-      {/* Navbar en la parte inferior */}
-      <Navbar />
+      {/* Navbar en la parte inferior, pasando la función correcta */}
+      <Navbar onAplicationClick={handleAplicationClick} />
     </div>
   );
 }
